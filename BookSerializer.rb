29db140c51293229268f681c8b244505477fb8f9
nvoulgaris <nvoulgaris@vivantehealth.com>
2018-11-18 13:20:35 +0200

    Implement a Serializer for books

---
+++ BookSerializer.rb
@@ -0,0 +1,17 @@
+class BookSerializer
+  def initialize(book)
+    @book = book
+  end
+
+  def as_json(*)
+    data = {
+      id:@book.id.to_s,
+      title:@book.title,
+      author:@book.author,
+      isbn:@book.isbn
+    }
+
+    data[:errors] = @book.errors if @book.errors.any?
+    data
+  end
+end
--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require "sinatra/namespace"
 require'mongoid'
 
 require './Book'
+require './BookSerializer'
 
 Mongoid.load! "mongoid.config"
 
@@ -16,9 +17,9 @@ namespace '/api/v1' do
     books = Book.all
 
     [:title, :isbn, :author].each do |filter|
-      books = books.send(filter, params[filter]) if params[filter]
+      books = books.send(filter, ptharams[filter]) if params[filter]
     end
 
-    books.to_json
+    books.map { |book| BookSerializer.new(book) }.to_json
   end
 end
